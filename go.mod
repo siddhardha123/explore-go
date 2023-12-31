aeb7f4cd15d27bbb8cc53f5da94d30b1ffa74466
siddhardha123 <siddhardha123123@gmail.com>
2023-12-31 21:36:07 +0530

    🚀basics

---
+++ go.mod
@@ -0,0 +1,3 @@
+module firstgomodule
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,24 @@
+package main 
+import "fmt"
+
+func main (){
+
+    fmt.Println("Hello World")
+
+	//variable 
+	var sid int = 10
+	//variable name type = value
+	fmt.Println(sid)
+
+	// runes in go
+
+	var a bool = true
+     fmt.Println(a)
+
+	 b := a 
+
+	 fmt.Println(b)
+}
+
+
+
